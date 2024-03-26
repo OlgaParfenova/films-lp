e5dfbd4c1cc8a7de953c798cb3c819931be6c59e
Olga Parfenova <work.parfenova@gmail.com>
2024-03-26 14:03:48 +0300

    Search functionality has been added to the Main page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,4 +1,6 @@
 import { Spin } from 'antd';
+import { ChangeEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useGetNowPlayingFilmsQuery } from '../../API/nowPlayingFilmsApi/getNowPlayingFilmsEndpoint';
 import {
   Button,
@@ -8,10 +10,24 @@ import {
   SearchInput,
   Title,
 } from '../../components';
+import { routes } from '../../router/routes';
 import styles from './MainPage.module.css';
 
 export const MainPage = () => {
+  const navigate = useNavigate();
+  const [searchQuery, setSearchQuery] = useState('');
   const { data: filmsData, isLoading } = useGetNowPlayingFilmsQuery();
+
+  const handleSearchButtonClick = () => {
+    navigate({
+      pathname: routes.searchFilmsPageURL,
+      search: `?query=${searchQuery}`,
+    });
+  };
+
+  const handleSearchInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
 
   if (!filmsData || (!filmsData && isLoading))
     return (
@@ -30,10 +46,15 @@ export const MainPage = () => {
           </Paragraph>
         </div>
         <div className={styles['mainPage__search-block']}>
-          <SearchInput placeholder='Search for a movie, tv show, person ...' />
+          <SearchInput
+            placeholder='Search for a movie, tv show, person ...'
+            onChange={handleSearchInput}
+            value={searchQuery}
+          />
           <Button
             fill='secondaryFill'
-            className={styles['mainPage__search-block__button']}>
+            className={styles['mainPage__search-block__button']}
+            onClick={handleSearchButtonClick}>
             Search
           </Button>
         </div>
